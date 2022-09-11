20d613121c4203af54674a5b4ec3dd71fe116d7a
valmir <valmirchagas88@gmail.com>
2022-09-10 23:57:03 -0300

    Iniciando exercícios python day 4

---
+++ day-4/exemplos.py
@@ -0,0 +1,18 @@
+''' Esse é um módulo de exemplos'''
+import random
+# Número inteiro aleaatório
+radom_number = random.randint(1, 10)
+print(radom_number)
+
+# Número float aleatório
+float_random = random.random()
+print(float_random)
+
+# Número float até valor determinado
+float_random = random.random() * 5
+print(float_random)
+
+# Número aleatório inteiro dentro de um range
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
