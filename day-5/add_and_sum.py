e8da7e7e299f22cf57c922f216619a4814103c8b
valmir <valmirchagas88@gmail.com>
2022-09-16 13:09:31 -0300

    exercicio add and sum number on loop

---
+++ day-5/add_and_sum.py
@@ -0,0 +1,14 @@
+'''Adding Even Numbers'''
+
+# TOTAL = 0
+# for number in range(1, 101):
+#     if number % 2 == 0:
+#         TOTAL += number
+
+# print(TOTAL)
+
+TOTAL = 0
+for number in range(2, 101, 2):
+    TOTAL += number
+
+print(TOTAL)
