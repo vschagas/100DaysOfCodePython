0b9bbcd3dfa96a54af49b1589d859a1e6c59d59b
valmir <valmirchagas88@gmail.com>
2022-10-03 10:48:44 -0300

    Exercise to pratice declare functions

---
+++ day-6/drawSquare.py
@@ -0,0 +1,17 @@
+'''exercise at site reeborg.ca'''
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+def turn_rigth():
+  turn_left()
+  turn_left()
+  turn_left()
+
+turn_left()
+move()
+turn_rigth()
+move()
+turn_rigth()
+move()
+turn_rigth()
+move()
