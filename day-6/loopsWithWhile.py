ca4361b78f7dd2cc65e5d94826fb34c476e55b99
valmir <valmirchagas88@gmail.com>
2022-10-19 10:10:23 -0300

    Loops with while

---
+++ day-6/loopsWithWhile.py
@@ -0,0 +1,24 @@
+'''exercise at site reeborg.ca'''
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+
+
+def turn_rigth():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_rigth()
+    move()
+    turn_rigth()
+    move()
+    turn_left()
+    
+    
+while at_goal() != True:
+    jump()
+    
